144ae83a98299a97b26abd471c9182b4f6f31d41
xmtdnmsl <3912831893@qq.com>
2025-07-08 06:41:09 +0800

    Add files via upload

--- FengYu.lua
+++ FengYu.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xmtdnmsl/-1/refs/heads/main/UI.lua"))()     
+local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/djdu894/ui/refs/heads/main/ui.lua"))();     
 local win = library:new("JOJO脚本")
 --
 local UITab1 = win:Tab("『信息』",'16060333448')
