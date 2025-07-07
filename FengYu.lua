71242fe3eabbf3dce3911ab5b3e32a8204274833
xmtdnmsl <3912831893@qq.com>
2025-07-08 05:41:48 +0800

    Add files via upload

--- FengYu.lua
+++ FengYu.lua
@@ -34,6 +34,22 @@ if Value then
 		end
 end)
 
+about:Toggle("自动互动", "Auto Interact", false, function(state)
+        if state then
+            autoInteract = true
+            while autoInteract do
+                for _, descendant in pairs(workspace:GetDescendants()) do
+                    if descendant:IsA("ProximityPrompt") then
+                        fireproximityprompt(descendant)
+                    end
+                end
+                task.wait(0.25) -- Adjust the wait time as needed
+            end
+        else
+            autoInteract = false
+        end
+end)
+
 local UITab1 = win:Tab("『绘制』",'16060333448')
 
 local about = UITab1:section("『玩家绘制』",false)
