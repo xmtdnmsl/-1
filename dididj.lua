0cf62bd95179bd30ed46fc3bc1937b9e284e596e
xmtdnmsl <3912831893@qq.com>
2025-07-10 13:44:08 +0800

    Add files via upload

--- dididj.lua
+++ dididj.lua
@@ -90,23 +90,24 @@ Tabs.SpeedTab:Button({
     Title = "飞行",
     Desc = "飞行",
     Callback = function()
-        local code = [[
-            local args = {"collectOrb", "Red Orb", "City"}
-            while true do
-                game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
-                task.wait(0.0001)
-            end
-        ]]
         loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
     end
 })
 
-Tabs.SpeedTab:Button({
+Tabs.SpeedTab:Toggle({
     Title = "连跳",
     Desc = "连跳",
-    Callback = function()
+    Callback = function(Value)
     loadstring(game:HttpGet("https://pastebin.com/raw/V5PQy3y0", true))()
     end
 })
 
+Tabs.AddTextbox:Textbox({
+	Title = "跳跃高度设置",
+	Desc = "高度",
+	TextDisappear = true,
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+	end
+})
 
