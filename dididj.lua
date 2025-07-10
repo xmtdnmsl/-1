700111fbdf4a120afe1b26a7af937a5c9adb0b0f
xmtdnmsl <3912831893@qq.com>
2025-07-10 13:28:33 +0800

    Add files via upload

--- dididj.lua
+++ dididj.lua
@@ -77,152 +77,43 @@ Window:EditOpenButton({
     Draggable = true,
 })
 
-local Tab = Window:MakeTab({
-	Name = "玩家通用设置",
-	Icon = "rbxassetid://7733779610",
-	PremiumOnly = false
-})
+local Tabs = {}
 
-Tab:AddButton({
-	Name = "电脑键盘",
-	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
-  	end    
-})
-
-Tab:AddButton({
-  Name = "HUA 光影",
-  Callback = function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/arzRCgwS"))()
-  end
-})
-
-Tab:AddButton({
-  Name = "光影_2",
-  Default = false,
-  Callback = function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MZEEN2424/Graphics/main/Graphics.xml"))()
-  end
-})
-
-Tab:AddButton({
-	Name = "超高画质",
-	Callback = function()
-loadstring(game:HttpGet("https://pastebin.com/raw/jHBfJYmS"))()
+do
+    Tabs.MainTab = Window:Section({Title = "通用脚本", Opened = true})
+    Tabs.SpeedTab = Tabs.MainTab:Tab({ Title = "通用", Icon = "zap" })
 end
-})    
 
-Tab:AddButton({
-	Name = "FPS",
-	Callback = function()
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/gclich/FPS-X-GUI/main/FPS_X.lua"))()
-  	end    
-})
+Window:SelectTab(1)
 
-Tab:AddButton({
-  Name = "旋转哦",
-  Callback = function()
-    loadstring(game:HttpGet('https://pastebin.com/raw/r97d7dS0', true))()
-  end
-})
-
-Tab:AddButton({
-	Name = "旋转转转起来～",
-	Callback = function()
-      	loadstring(game:HttpGet('https://pastebin.com/raw/r97d7dS0', true))()
-  	end
-})
-
-Tab:AddToggle({
-	Name = "夜视功能👍",
-	Default = false,
-	Callback = function(Value)
-		if Value then
-		    game.Lighting.Ambient = Color3.new(1, 1, 1)
-		else
-		    game.Lighting.Ambient = Color3.new(0, 0, 0)
-		end
-	end
-})
-
-Tab:AddButton({
-    Name = "好玩的动作",
+Tabs.SpeedTab:Button({
+    Title = "飞行",
+    Desc = "飞行",
     Callback = function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/Zj4NnKs6"))()
+        local code = [[
+            local args = {"collectOrb", "Red Orb", "City"}
+            while true do
+                game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+                task.wait(0.0001)
+            end
+        ]]
+        loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
     end
 })
 
-Tab:AddButton({
-	Name = "踏空走路",
-	Callback = function()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Float'))()
-	end
+Tabs.SpeedTab:Button({
+    Title = "飞行",
+    Desc = "飞行",
+    Callback = function()
+        local code = [[
+            local args = {"collectOrb", "Red Orb", "City"}
+            while true do
+                game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+                task.wait(0.0001)
+            end
+        ]]
+        loadstring(game:HttpGet("https://pastebin.com/raw/V5PQy3y0", true))()
+    end
 })
 
-Tab:AddButton({
-	Name = "点击传送工具(推荐!!!)",
-	Callback = function()
-mouse = game.Players.LocalPlayer:GetMouse() tool = Instance.new("Tool") tool.RequiresHandle = false tool.Name = "[FE] TELEPORT TOOL" tool.Activated:connect(function() local pos = mouse.Hit+Vector3.new(0,2.5,0) pos = CFrame.new(pos.X,pos.Y,pos.Z) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos end) tool.Parent = game.Players.LocalPlayer.Backpack
-	end
-})
 
-Tab:AddTextbox({
-	Name = "跳跃高度设置",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-	end
-})
-
-Tab:AddTextbox({
-	Name = "移动速度设置(小心调太高了会飞出去哦)",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-	end
-})
-
-Tab:AddTextbox({
-	Name = "重力设置",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		game.Workspace.Gravity = Value
-	end
-})
-
-Tab:AddToggle({
-	Name = "穿墙",
-	Default = false,
-	Callback = function(Value)
-		if Value then
-		    Noclip = true
-		    Stepped = game.RunService.Stepped:Connect(function()
-			    if Noclip == true then
-				    for a, b in pairs(game.Workspace:GetChildren()) do
-                        if b.Name == game.Players.LocalPlayer.Name then
-                            for i, v in pairs(game.Workspace[game.Players.LocalPlayer.Name]:GetChildren()) do
-                                if v:IsA("BasePart") then
-                                    v.CanCollide = false
-                                end
-                            end
-                        end
-                    end
-			    else
-				    Stepped:Disconnect()
-			    end
-		    end)
-	    else
-		    Noclip = false
-	    end
-	end
-})
-
-Tab:AddButton({
-	Name = "飞行",
-	Callback = function()
-loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
-  	end
-})
