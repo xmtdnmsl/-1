e2fb14794c06021d1bf1d3d71187fc618acfacde
xmtdnmsl <3912831893@qq.com>
2025-07-10 20:15:38 +0800

    Add files via upload

--- 脚本.lua
+++ 脚本.lua
@@ -80,23 +80,174 @@ Window:EditOpenButton({
 local Tabs = {}
 
 do
-    Tabs.MainTab = Window:Section({Title = "极速传奇", Opened = true})
-    Tabs.SpeedTab = Tabs.MainTab:Tab({ Title = "刷", Icon = "zap" })
+    WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+function gradient(text, startColor, endColor)
+    local result = ""
+    local length = #text
+
+    for i = 1, length do
+        local t = (i - 1) / math.max(length - 1, 1)
+        local r = math.floor((startColor.R + (endColor.R - startColor.R) * t) * 255)
+        local g = math.floor((startColor.G + (endColor.G - startColor.G) * t) * 255)
+        local b = math.floor((startColor.B + (endColor.B - startColor.B) * t) * 255)
+
+        local char = text:sub(i, i)
+        result = result .. "<font color=\"rgb(" .. r ..", " .. g .. ", " .. b .. ")\">" .. char .. "</font>"
+    end
+
+    return result
+end
+
+local Confirmed = false
+
+WindUI:Popup({
+    Title = "欢迎进入冷寂脚本! 记得进群",
+    Icon = "rbxassetid://112136634870518",
+    IconThemed = true,
+    Content = "This is an Example UI for the " .. gradient("WindUI", Color3.fromHex("#00FF87"), Color3.fromHex("#60EFFF")) .. " Lib",
+    Buttons = {
+        {
+            Title = "Cancel",
+            Callback = function() end,
+            Variant = "Secondary",
+        },
+        {
+            Title = "Continue",
+            Icon = "arrow-right",
+            Callback = function() Confirmed = true end,
+            Variant = "Primary",
+        }
+    }
+})
+
+repeat wait() until Confirmed
+
+local Window = WindUI:CreateWindow({
+    Title = "冷寂",
+    Icon = "rbxassetid://129260712070622",
+    IconThemed = true,
+    Author = "冷寂脚本",
+    Folder = "CloudHub",
+    Size = UDim2.fromOffset(580, 460),
+    Transparent = true,
+    Theme = "Dark",
+    User = {
+        Enabled = true,
+        Callback = function() print("clicked") end,
+        Anonymous = true
+    },
+    SideBarWidth = 200,
+    ScrollBarEnabled = true,
+    KeySystem = {
+        Key = { "1234", "5678" },
+        Note = "Example Key System. \n\nThe Key is '1234' or '5678",
+        URL = "link-to-linkvertise-or-discord-or-idk",
+        SaveKey = true,
+    },
+})
+
+Window:CreateTopbarButton("MyCustomButton1", "bird", function() print("clicked 1!") end, 990)
+Window:CreateTopbarButton("MyCustomButton3", "battery-plus", function() Window:ToggleFullscreen() end, 989)
+
+Window:EditOpenButton({
+    Title = "冷寂脚本",
+    Icon = "monitor",
+    CornerRadius = UDim.new(0,16),
+    StrokeThickness = 2,
+    Color = ColorSequence.new(Color3.fromHex("FF0F7B"), Color3.fromHex("F89B29")),
+    Draggable = true,
+})
+
+local Tabs = {}
+
+do
+    Tabs.MainTab = Window:Section({Title = "通用脚本", Opened = true})
+    Tabs.SpeedTab = Tabs.MainTab:Tab({ Title = "通用", Icon = "zap" })
 end
 
 Window:SelectTab(1)
 
 Tabs.SpeedTab:Button({
-    Title = "kl刷速度",
-    Desc = "刷速度",
+    Title = "飞行",
+    Desc = "飞行",
     Callback = function()
-        local code = [[
-            local args = {"collectOrb", "Red Orb", "City"}
-            while true do
-                game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
-                task.wait(0.0001)
-            end
-        ]]
-        loadstring(code)()
+        loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+    end
+})
+
+Tabs.SpeedTab:Button({
+    Title = "连跳",
+    Desc = "连跳",
+    Callback = function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/V5PQy3y0", true))()
+    end
+})
+
+Tabs.SpeedTab:Button({
+    Title = "踏空",
+    Desc = "踏空",
+    Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Float'))()        
+    end
+})
+
+Tabs.SpeedTab:Button({
+    Title = "爬墙",
+    Desc = "爬墙",
+    Callback = function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/zXk4Rq2r"))()        
+    end
+})
+
+Tabs.SpeedTab:Button({
+    Title = "光影",
+    Desc = "光影",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MZEEN2424/Graphics/main/Graphics.xml"))()        
+    end
+})
+
+Tabs.SpeedTab:Button({
+    Title = "键盘",
+    Desc = "键盘",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()        
+    end
+})
+
+Tabs.SpeedTab:Button({
+    Title = "自瞄",
+    Desc = "自瞄",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20Aimbot.lua"))() 
+    end
+})
+
+Tabs.SpeedTab:Button({
+    Title = "变流畅",
+    Desc = "变流畅",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/gclich/FPS-X-GUI/main/FPS_X.lua"))()        
+    end
+})
+
+Tabs.SpeedTab:Button({
+    Title = "子追",
+    Desc = "子追",
+    Callback = function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/1AJ69eRG"))()        
+    end
+})
+
+Tabs.SliderTab:Slider({
+    Title = "跳跃高度",
+    Value = {
+           Min = 1,
+           Max = 100,
+           Default = 75,
+    }
+    Callback = function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
     end
 })
